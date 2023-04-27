1c2ed60fede19d7cccc20f228f6706f1f0b57908
Antoniomorales17 <126864866+Antoniomorales17@users.noreply.github.com>
2023-04-27 19:36:49 +0000

    arrays

--- Antonio/exercises.js
+++ Antonio/exercises.js
@@ -1,22 +1,9 @@
-const myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam sit amet magna nec dictum. Ut viverra mollis purus vel porta. Aenean ut sem fermentum enim viverra interdum vehicula eu nibh. Nullam accumsan tortor eu turpis egestas, et maximus mauris elementum. Vivamus varius vel lacus non tincidunt. Aenean vel arcu arcu";
+
+const add =(firstNumber, secondNumber) => firstNumber + secondNumber;
+
+const substract =(firstNumber, secondNumber) => firstNumber - secondNumber;
+
+const multiply=  (firstNumber, secondNumber) => firstNumber * secondNumber;
 
 
-
-
-
-const stringo= myString.replaceAll("i","o");
-console.log(stringo);
-
-const words = myString.split("i");
-const wordslenght= words.length
-console.log(wordslenght);
-
-
-const index = myString.slice(19, 23)
-console.log(index);
-
-
-const lastExample ="   HOLA   "
-const exampleTrim = lastExample.trim();
-
-console.log(exampleTrim);
+const divide =(firstNumber, secondNumber) => firstNumber / secondNumber;

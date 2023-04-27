958b185bb677f9b1bd25e930e9b3c8f5b47828ea
Michel <49735520+michelcub@users.noreply.github.com>
2023-04-27 17:36:15 +0000

    functions

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function add(firstNum, secondNum){
+ return firstNum + secondNum;
+}
+
+function substract(firstNum, secondNum){
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum){
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum){
+    return firstNum / secondNum;
+}
+
+

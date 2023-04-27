326f45291d8abfa881e6fd4e1a300cb7a78c114d
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-04-27 17:40:43 +0000

    function.alex

---
+++ alex/calculator.js
@@ -0,0 +1,22 @@
+function add (firstNumber, secondNumber) {
+
+    return firstNumber + secondNumber;
+
+}
+
+function substract (firstNumber,secondNumber){
+    return firstNumber - secondNumber;
+}
+
+function multiply () {
+    return firstNumber* secondNumber;
+}
+
+function divide (firstNumber,secondNumber) {
+    return firstNumber / secondNumber;
+}
+
+
+let result = add(3,88);
+result = multiply(result,91);
+console.log(result);
---
+++ alex/function.js
@@ -0,0 +1,28 @@
+
+// notación tradicional
+
+
+
+function greet (name) {
+
+    console.log(`Hello, ${name}!`);
+
+}
+
+greet('Alex');
+
+function sum(a,b) {
+
+    console.log(a + b);
+
+}
+
+sum(3,4);
+sum(25,62);
+
+// function
+
+function sum (a + b) {
+    return a + b;
+
+}

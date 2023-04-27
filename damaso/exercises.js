92e55881bbac30e4776d0b4309d93e3067b3c4b0
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2023-04-27 18:15:51 +0000

    calculator

--- damaso/exercises.js
+++ damaso/exercises.js
@@ -1,13 +1,7 @@
-const myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam sit amet magna nec dictum. Ut viverra mollis purus vel porta. Aenean ut sem fermentum enim viverra interdum vehicula eu nibh. Nullam accumsan tortor eu turpis egestas, et maximus mauris elementum. Vivamus varius vel lacus non tincidunt. Aenean vel arcu arcu."
-const stringUp = myString.toUpperCase();
-const stringLower = myString.toLowerCase();
-const sting0 = myString.replaceAll('i', 'o');
+const add = (firstNum, secondNum) => firstNum + secondNum;
 
-console.log(string0);
+const substract = (firstNum, secondNum) => firstNum - secondNum;
 
-const words = myString.split("i");
-const wordsLength = words.length
-console.log(words);
+const multiply = (firstNum, secondNum) => firstNum * secondNum; 
 
-const index = myString.slice(19, 23);
-console.log(index);
+const divide = (firstNum, secondNum) => firstNum / secondNum;

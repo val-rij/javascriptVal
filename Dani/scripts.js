14364b0f674b8b6413797e22dd435d4fb2539bba
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-04-27 18:13:05 +0000

    Finish calculator practise

--- Dani/scripts.js
+++ Dani/scripts.js
@@ -1,48 +1,15 @@
+//Notación tradicional
 
-let myVariable = 10;
+const miNombre = "Dani"
 
+function greet (name) {
+    console.log(`Hello, ${name}`)
+}
 
-let regularUseCase = "Hello, world!";
+greet(miNombre);
 
-myVariable = regularUseCase;
+//Anonymous functions (Arrow functions)
 
+const myFirstArrowFunction = text => text.toLowerCase();
 
-//Constantes
-const myConstant = 1;
-
-//Tipos de datos
-//Booleans
-const firstBoolean = true;
-const secondBoolea = false;
-
-//String
-const singleQuoteString = 'Cadenas de caracteres';
-const doubleQuoteString = "Cadenas de caracteres";
-
-
-//Template literals
-const loginMessage = "Logout"
-const templateLiteral = `<button>${loginMessage}</button>`
-
-console.log (templateLiteral);
-
-//Concat
-const hello = "Hello,"
-const world = "world!"
-
-const concatHelloWorldMessage = hello + ' ' + world;
-const concatHelloWorld = `${hello} ${world}`;
-
-console.log(concatHelloWorld)
-
-
-//Numbers 
-const exampleNumber = 5;
-
-//undefined 
-const undefinedExample = undefined;
-
-//null
-const nullExample = null;
-
-console.log(nullExample)
+console.log(myFirstArrowFunction("HOLA!"));
